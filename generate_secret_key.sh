5ec1c302a3a5a73d1b063418bc55e141fb00b3a2
David Neale <neale.dj@gmail.com>
2016-08-30 22:06:12 +0100

    Remove secret key

--- generate_secret_key.sh
+++ generate_secret_key.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python -c "import os,binascii;print binascii.b2a_hex(os.urandom(32)).upper()" > checker/secret_key
+python -c "import os,binascii;print binascii.b2a_hex(os.urandom(32)).upper()" > appengine/secret_key
