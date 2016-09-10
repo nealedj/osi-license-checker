94fd15e4a1df7073717fc196b7a2ecc3d5e101de
David Neale <neale.dj@gmail.com>
2016-09-10 08:04:00 +0100

    Update npm processor to support explicit repo and version

--- checker/processors/npm.py
+++ checker/processors/npm.py
@@ -4,18 +4,19 @@ from .. import license_matcher
 from .base import BaseProcessor
 
 URL_DATA_PATTERN = re.compile(r'(www.)?npm(js)?(.com)?\/(package\/)?([\w\-\_]+)')
-JSON_DATA_PATTERN = 'http://registry.npmjs.org/{0.repo}/latest'
+JSON_DATA_PATTERN = 'http://registry.npmjs.org/{0.repo}/{0.version}'
 
 
 class NpmProcessor(BaseProcessor):
     name = 'npm'
 
-    def __init__(self, url):
+    def __init__(self, url=None, repo=None, version=None):
         self.url = url
-        self.repo = self.extract_url_data()[4]
+        self.repo = repo or self.extract_url_data()[4]
+        self.version = version or 'latest'
 
     def extract_url_data(self):
-        match = URL_DATA_PATTERN.match(self.url)
+        match = URL_DATA_PATTERN.search(self.url)
         return match.groups()
 
     @classmethod
@@ -23,7 +24,7 @@ class NpmProcessor(BaseProcessor):
         return URL_DATA_PATTERN.match(url)
 
     def get_license(self):
-        data_url = JSON_DATA_PATTERN.format(self),
+        data_url = JSON_DATA_PATTERN.format(self)
         repo_info = self.get_file_data(
             data_url,
             parse_json=True
