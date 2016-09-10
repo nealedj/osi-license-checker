2204cf5ef4bdb827c0b95284f0a1aab58ee806f3
David Neale <neale.dj@gmail.com>
2016-09-10 07:53:24 +0100

    Support pypi versions

--- checker/processors/pypi.py
+++ checker/processors/pypi.py
@@ -5,14 +5,16 @@ from .base import BaseProcessor
 
 URL_DATA_PATTERN = re.compile(r'(pypi.python.org\/)?pypi\/([\w\-\_]+)')
 JSON_DATA_PATTERN = 'http://pypi.python.org/pypi/{0.repo}/json'
+JSON_DATA_PATTERN_WITH_VERSION = 'http://pypi.python.org/pypi/{0.repo}/{0.version}/json'
 
 
 class PyPiProcessor(BaseProcessor):
     name = 'pypi'
 
-    def __init__(self, url):
+    def __init__(self, url=None, repo=None, version=None):
         self.url = url
-        self.repo = self.extract_url_data()[1]
+        self.repo = repo or self.extract_url_data()[1]
+        self.version = version
 
     def extract_url_data(self):
         match = URL_DATA_PATTERN.match(self.url)
@@ -23,11 +25,45 @@ class PyPiProcessor(BaseProcessor):
         return URL_DATA_PATTERN.match(url)
 
     def get_license(self):
-        data_url = JSON_DATA_PATTERN.format(self)
+        repo_info, data_url = self.get_repo_info()
 
-        repo_info = self.get_file_data(
-            data_url,
-            parse_json=True
-        )
+        if repo_info:
+            license = repo_info.get('license')
 
-        return repo_info['info'].get('license'), data_url
+            if not license:
+                license = self.get_license_from_classifiers(repo_info)
+                data_url += ' (from classifiers)'
+        else:
+            license = None
+
+        return license, data_url
+
+    def get_repo_info(self):
+        file_data = None
+
+        if self.version:
+            # try and get the version-specific info
+            data_url = JSON_DATA_PATTERN_WITH_VERSION.format(self)
+            file_data = self.get_file_data(
+                data_url,
+                parse_json=True
+            )
+
+        if not file_data:
+            data_url = JSON_DATA_PATTERN.format(self)
+            file_data = self.get_file_data(
+                data_url,
+                parse_json=True
+            )
+
+        if file_data:
+            return file_data.get('info'), data_url
+
+    def get_license_from_classifiers(self, repo_info):
+        classifiers = repo_info['classifiers']
+
+        pattern = r"License :: OSI Approved :: ([\w\s]*)"
+        for classifier in classifiers:
+            match = re.search(pattern, classifier)
+            if match and match.groups():
+                return match.groups()[0]
